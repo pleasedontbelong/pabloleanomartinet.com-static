40ee14e35fef7e4664ff3a84b36998ede9cd3dce
Pablo Leano <pleasedontbelong@gmail.com>
2015-06-08 18:10:49 +0200

    Fix gems and basic sass structure

--- plm_website/apps/default_theme/static/config.rb
+++ plm_website/apps/default_theme/static/config.rb
@@ -1,12 +1,13 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+require 'sass-globbing'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "css"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "css/"
+sass_dir = "css/"
+images_dir = "images/"
+javascripts_dir = "js/"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed

79efccb9351cd78be7aceec35fe9ba8f01fbe082
Pablo Leano <pleasedontbelong@gmail.com>
2015-07-05 23:36:51 +0200

    Draft post description on template

--- plm_website/apps/blog/templates/posts/adding-custom-admin-pages-django_meta.py
+++ plm_website/apps/blog/templates/posts/adding-custom-admin-pages-django_meta.py
@@ -3,4 +3,5 @@ from datetime import datetime
 CATEGORY = "mycat"
 AUTHOR = "Pablo"
 POST_TITLE = "Adding custom admin pages on django"
+POST_DESCRIPTION = "Adding custom admin pages on django"
 PUBLISHED_DATE = datetime(year=2015, month=3, day=24)

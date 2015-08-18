f4d2424435e2e7548cea62d9462a558f9bb4d9ba
Pablo Leano <pleasedontbelong@gmail.com>
2015-08-18 19:22:29 +0200

    Finish mock post

---
+++ plm_website/apps/content/templates/posts/test-and-mock-a-property-class-in-python_meta.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from .constants import CATEGORIES
+
+CATEGORY = CATEGORIES.POST
+AUTHOR = "Pablo"
+POST_TITLE = "Test and mock a Class property in python"
+POST_DESCRIPTION = "How to mock a class property in python for unit tests"
+
+POST_TAGS = ("python", "mock", "property")
+
+PUBLISHED_DATE = datetime(year=2015, month=8, day=18)

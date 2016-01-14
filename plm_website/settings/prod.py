5e55afd9797b064253c8132c294ce4ed9a48d138
Pablo Leano <pleasedontbelong@gmail.com>
2016-01-14 13:40:53 +0100

    Add logging to production

--- plm_website/settings/prod.py
+++ plm_website/settings/prod.py
@@ -5,3 +5,15 @@ from .base import *
 ALLOWED_HOSTS = [os.getenv('VIRTUAL_HOST', 'localhost')]
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',  # Not recommended.
+        }
+    },
+}

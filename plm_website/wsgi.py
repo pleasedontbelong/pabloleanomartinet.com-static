69a15f93e773c878e36a9a760f256d7e00bee46c
Pablo Leano <pleasedontbelong@gmail.com>
2016-02-09 17:34:20 +0100

    Fix import

--- plm_website/wsgi.py
+++ plm_website/wsgi.py
@@ -6,6 +6,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import newrelic
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plm_website.settings")
 
 from django.core.wsgi import get_wsgi_application

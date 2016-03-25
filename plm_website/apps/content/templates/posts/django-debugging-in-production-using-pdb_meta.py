adc44da2911bd36ab76af297bc7679c12516a00b
Pablo Leano <pleasedontbelong@gmail.com>
2016-03-25 15:36:25 +0100

    Add pdb post

---
+++ plm_website/apps/content/templates/posts/django-debugging-in-production-using-pdb_meta.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from .constants import CATEGORIES
+
+CATEGORY = CATEGORIES.PROJECT
+AUTHOR = "Pablo"
+POST_TITLE = "Debugging Django in production using pdb"
+POST_DESCRIPTION = "A quick guide trick to be able to debug on a production server."
+
+POST_TAGS = ("Django", "pdb", "debug", "debugging", "trick")
+
+PUBLISHED_DATE = datetime(year=2016, month=3, day=25)

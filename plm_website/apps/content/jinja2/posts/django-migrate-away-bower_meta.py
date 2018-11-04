fe1165e7d8101f1650f4b9609f9d055074a14439
Pablo Leano <pleasedontbelong@gmail.com>
2018-11-04 19:58:21 +0100

    Add django bower post

---
+++ plm_website/apps/content/jinja2/posts/django-migrate-away-bower_meta.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from .constants import CATEGORIES
+
+CATEGORY = CATEGORIES.POST
+AUTHOR = "Pablo"
+POST_TITLE = "Migrating away from bower in a django project"
+POST_DESCRIPTION = "My experience migrating away from bower in my" \
+                   " django project."
+
+POST_TAGS = ("django", "jinja2", "bowe", "migration", "webpack", "npm")
+
+PUBLISHED_DATE = datetime(year=2018, month=11, day=4)
